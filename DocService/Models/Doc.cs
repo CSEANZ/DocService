21775fbf196d1dc8d7f2608a78d236ab6911e193
Andrew Coates <acoat@microsoft.com>
2016-11-09 10:11:34 +1100

    Required properties in models

--- DocService/Models/Doc.cs
+++ DocService/Models/Doc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace DocService.Models
 {
     public class Doc
     {
+        [Required()]
         public string Title { get; set; }
         public string FileName { get; set; }
         public Guid Id { get; set; }
--- DocService/Models/Para.cs
+++ DocService/Models/Para.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace DocService.Models
     {
         public Guid Id { get; set; }
         public Guid? DocId { get; set; }
+        [Required()]
         public string Text { get; set; }
         public string Style { get; set; }
         public int Order { get; set; }
